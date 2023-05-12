5d5ea5d931de36e8f123a3ff14129a5027ccb9d5
Kakon Debnath <kakondebnath96@gmail.com>
2023-05-12 07:45:29 +0600

    add vercel json

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        // await client.connect();
+        await client.connect();
         const serviceCollections = client.db("carDoctorDB").collection("services");
         const orderCollections = client.db("carDoctorDB").collection("orders");
 
